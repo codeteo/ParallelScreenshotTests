f2c5dd27123886f151c23b26b31b76a8247fe736
tioakeimidis <tioakeimidis@trading-point.com>
2022-02-04 16:48:10 +0200

    Success with sshot test

--- app/src/androidTest/java/com/parallel/shots/FragmentScreenshotTest.kt
+++ app/src/androidTest/java/com/parallel/shots/FragmentScreenshotTest.kt
@@ -1,19 +1,24 @@
 package com.parallel.shots
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
 import androidx.fragment.app.testing.launchFragmentInContainer
 import com.karumi.shot.FragmentScenarioUtils.waitForFragment
 import com.karumi.shot.ScreenshotTest
 import org.junit.Test
 
 class FragmentScreenshotTest : ScreenshotTest {
-    private var fragmentScenario =
-        launchFragmentInContainer<FirstFragment>().waitForFragment()
 
     @Test
-    fun theActivityIsShownProperly() {
-        val fragment = fragmentScenario
+    fun basicScenarioWithDefaultArguments() {
+        val fragment = launchFragmentInContainer<FirstFragment>(
+            factory = object : FragmentFactory() {
+                override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
+                    return FirstFragment()
+                }
+            }
+        ).waitForFragment()
 
         compareScreenshot(fragment)
     }
-
 }
